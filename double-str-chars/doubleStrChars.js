8a7d826a89c9fe53bd3f31605fd0c489c79bb8c0
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-05 13:12:26 -1000

    Finished doubleStrChars one try! yasss!

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,14 @@
 module.exports = function doubleStrChars(str) {
   // write your code in here
+  
+  var newString = '';
+  
+  if (typeof str !== 'string'){
+    return 'not a string!';
+  } else{
+    for (var i = 0; i < str.length; i++){
+      newString += str.charAt(i)+str.charAt(i);
+    }
+  }
+  return newString;
 }
