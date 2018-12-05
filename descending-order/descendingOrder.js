0285d17d8769d49bfb47bf0f4707083510f7aa8f
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-05 10:57:47 -1000

    Finished Descending Order- could not figure out how to put the for loop in after the if statement

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,19 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+  var newDescenOrder = null
+  var newArr = []
+
+  console.log(typeof number);
+
+  if (typeof number === 'number'){
+    newArr = Array.from(number.toString()).sort(function(a,b){return b-a});
+    newDescenOrder = newArr.join('')ç;
+    return newDescenOrder;
+
+
+    console.log(newArr);
+    } 
+    else{
+      return 'not a number!';
+    }
 }
