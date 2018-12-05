9ea3c843e6fd0648c06c11d3b5634669857e808d
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-05 09:07:54 -1000

    Finished counByMultiples, first try! woot! :)

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,11 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+var newMultipleArr = [x];
+var newNum = x;
+
+for( var i = 0; i < (length - 1); i++){
+  newNum += x
+  newMultipleArr.push(newNum);
+}
+return newMultipleArr;
 }
