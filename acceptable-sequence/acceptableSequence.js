b5e29480ab5de776167b40e1b23fbf371d623bb4
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2018-12-04 21:49:06 -1000

    change return to hold variable

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,7 +1,18 @@
 module.exports = function acceptableSequence(str) {
   // write your code here
-  for (var i = 0; i < str.length; i ++);{
-    if (str[i])
-  }
 
+  for (var i = 0; i < str.length; i++){
+    
+    if(/\w/.test(str.charAt(i))){
+      if(/\w/.test(str.charAt(0))){
+        return false;
+      }
+      else if (str.charAt(i+1) === '+' && str.charAt(i-1) === '+'){
+      return true;
+      //create variable hold true or false.
+      } else { 
+      return false;
+      }
+    }
+  }
 }
