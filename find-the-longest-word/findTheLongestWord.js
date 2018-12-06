47d796f138664821f43e8cd6c9509528a347b088
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-06 12:55:09 -1000

    Finished findTheLongestWord.  Must remember to put the return outside of the for Loop.

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,18 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+
+  var longestWord = '';
+
+  for (var i = 0; i < sen.length; i++){
+    if (sen[i].length > longestWord.length){
+      longestWord = sen[i];
+    } else {
+      continue;
+    }
+
+  }
+  return longestWord;
+
+
+
 }
