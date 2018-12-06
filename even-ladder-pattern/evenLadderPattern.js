1570e433764d3d747b77e37b19381cf5dcd2e9f2
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-05 19:42:50 -1000

    Finished evenLadderPattern. Works but too complex, must have more simple answer

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,19 @@
 module.exports = function evenLadderPattern(num) {
   // write your code in here
+
+var newString = '';
+
+if (num <= 1){
+  return '';
+} else {
+  for (var i = 2; i <= num; i++){
+    if ((num - i) >= 2){
+    newString += String(i).repeat(i) + '\n';
+    i += 1;
+    }else{
+      newString += String(i).repeat(i);
+    i += 1;
+    }
+  }return newString;
+}
 }
