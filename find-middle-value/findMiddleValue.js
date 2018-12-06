b070cd10a7faf81cc65aff5937647587a6142f9b
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-06 12:45:36 -1000

    finished findMiddleValue . Could use if statement or case statement also. used Math.max/min.

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,3 +1,14 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
+
+  maxNumIdx = numArray.indexOf(Math.max.apply(null,numArray));
+  minNumIdx = numArray.indexOf(Math.min.apply(null,numArray));
+
+  console.log(maxNumIdx);
+  console.log(minNumIdx);
+  
+  desiredIndex = (3 - (maxNumIdx + minNumIdx));
+
+  return desiredIndex;
+
 }
