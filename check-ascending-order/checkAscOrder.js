b106521a826b5db864f3bd2c69fdff0bcf8d4c21
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-06 17:53:53 -1000

    was only checking the first value. added a variable to check the other values up to the last value.

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,11 +1,15 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
-for (var i = 0; i < numArray.length; i++){
+
+  var answer = null;
+
+
+  for (var i = 0; i < numArray.length - 1; i++){
   if (numArray[i] < numArray[i+1]){
-    return true;
+    answer =  true;
   }
   else{
     return false;
   }
-}
+}return answer;
 }
--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,14 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+
+  var strayValue = '';
+
+for ( i = 0; i < array.length; i++){
+  if (array[i] !== array [i + 1]){
+    strayValue = array[i];
+  } else{
+    continue;
+  }
+}
+return strayValue;
 }
