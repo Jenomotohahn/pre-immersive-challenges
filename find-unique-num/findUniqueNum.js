2094737e1273334ab12ba21f7049ca47b92d7b21
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 02:22:42 -1000

    Finished FindUniqueNum but too long of code. Probably could refactor

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,27 @@
 module.exports = function findUniqueNum(strOfNums) {
   // write your code in here
-}
+
+  var numArr = strOfNums.split(' ');
+  var evenOddArr =[];
+  
+  for (var i = 0; i < numArr.length; i++){
+    if(numArr[i] % 2 === 0){
+      evenOddArr.push('0')
+    } else{
+      evenOddArr.push('1');
+    }
+  }
+  
+  for (var i = 0; i < evenOddArr.length; i++){
+    if ((evenOddArr[i] === evenOddArr[i +1]) && (evenOddArr[i] === evenOddArr[i +2])){
+      continue;
+    } else if((evenOddArr[i] !== evenOddArr[i+1]) && ((evenOddArr[i]) === evenOddArr[i +2])){
+      return evenOddArr.indexOf(evenOddArr[i+1]);
+    } else if((evenOddArr[i] === evenOddArr[i+1]) && ((evenOddArr[i] !== evenOddArr[i+2]))){
+      return evenOddArr.indexOf(evenOddArr[i+2]);
+    } else{
+      return evenOddArr.indexOf(evenOddArr[i]);
+    }
+  }
+
+} 
