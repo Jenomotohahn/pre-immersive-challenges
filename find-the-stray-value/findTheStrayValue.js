4298026fb1022c2b5c87e7d875e0ab1e8029e4a5
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 01:22:47 -1000

    Finished findTheStrayValue

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,18 +1,16 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
 
- var strayValue = '';
- var stableValue = '';
 
- for (var i = 0; i < array.length; i++){
- 
-
-
-
-
-
+for (var i = 0; i < array.length; i++){
+  if((array[i] === array[i+1]) && (array[i] === array[i +2])){
+    continue;
+  } else if ((array[i] !== array[i + 1]) && (array[i] === array[i + 2])){
+    return array[i +1];
+  } else if((array[i] === array[i +1]) && (array[i] !== array[i +2])){
+    return array[i+2];
+  } else{
+    return array[i];
+   }
+  }
  }
-
-
-
-}
