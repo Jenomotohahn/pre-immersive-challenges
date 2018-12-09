3cfffc5b7708635c99ddbcf269fc21cb88ee7602
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 13:14:06 -1000

    Finished insertDashes

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,20 @@
 module.exports = function insertDashes(num) {
   // write your code in here
+
+newStr = num.toString();
+evenDash = '';
+
+for (var i = 0; i < newStr.length; i++){
+  if ((newStr.charAt(i+1) === '')){
+    evenDash += newStr.charAt(i);
+  } else if ((newStr.charAt(i) % 2 === 0) && (newStr.charAt(i + 1) % 2 === 0)){
+    evenDash += newStr.charAt(i) + '-';
+  } else {
+    evenDash += newStr.charAt(i);
+  }
+} return evenDash;
+
+
+
+
 }
