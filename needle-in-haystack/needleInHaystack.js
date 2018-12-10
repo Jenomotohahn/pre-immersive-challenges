cfe6c3267df039b44d0c2e396720c8ab6b9ffa88
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 20:36:19 -1000

    Finished reverseMessage.  Alternative method, put to arrays to reverse and rejoin.

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,10 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
+
+  if(haystack.indexOf('needle') !== -1){
+    return haystack.indexOf('needle');
+  } else{
+    return false;
+  }
+
 }
--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -1,3 +1,18 @@
 module.exports = function reverseMessage(str) {
   // write your code in here
+
+ var finalStr = '';
+
+ for( var i = 0; i < str.length; i++){
+    if ((i === str.length - 1) || ((str.charAt(i + 1) === ' ') && (i !== 0))){
+      finalStr = str.charAt(i).toUpperCase() + finalStr;
+    } else {
+      finalStr = str.charAt(i).toLowerCase() + finalStr;
+
+    } 
+  }return finalStr;
+
 }
+
+
+
