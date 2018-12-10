705e1069e8c81cc46bf9794c5fc759e40f93779c
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 20:57:51 -1000

    Finished swapLetterCase. Too wordy, but works. Could use refactoring.

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,18 @@
 module.exports = function swapLetterCase(str) {
   // write your code in here
+
+  var newStr = '';
+
+  for (var i = 0; i < str.length; i++){
+    if((str.charAt(i).toUpperCase() === str.charAt(i)) && (str.charAt(i).match(/[a-z]/i))){
+      newStr += str.charAt(i).toLowerCase();
+    } else if (((str.charAt(i).toLowerCase() === str.charAt(i))) && ((str.charAt(i).match(/[a-z]/i)))){
+      newStr += str.charAt(i).toUpperCase();
+    } else if (str.charAt(i) === ' '){
+      newStr += ' ';
+    } else{
+      newStr += str.charAt(i);
+    }
+  } return newStr;
+  
 }
