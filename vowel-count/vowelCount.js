5eb83e5725f8ea2682b26c13321afb20ab3e4f10
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 21:04:48 -1000

    Finished vowelCount.  Alternative ways possibly use .match

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,14 @@
 module.exports = function vowelCount(str) {
   // write your code in here
+
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var countVowels = 0;
+
+  for (var i = 0; i < str.length; i++){
+    if((vowels.indexOf(str.charAt(i)) !== -1)){
+      countVowels += 1;
+    } else{
+      continue;
+    }
+  } return countVowels;
 }
