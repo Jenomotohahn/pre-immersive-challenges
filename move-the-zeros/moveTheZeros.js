d71ddf519e84f97a2f5874e32786bf3de07aea3e
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-12-09 16:23:36 -1000

    Finished moveTheZeros

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,22 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
   // write your code here
+var newArr = [];
+var zeroArr = [];
+
+for (var i =0; i < arrNum.length; i++){
+  if (arrNum[i] === 0){
+    zeroArr.push(arrNum[i]);
+  }else{
+    newArr.push(arrNum[i]);
+  }
+}
+if(isRight === false){
+  var zeroToLeft = zeroArr.concat(newArr);
+  return zeroToLeft;
+} else if( isRight === true){
+  var zeroToRight = newArr.concat(zeroArr);
+  return zeroToRight;
+  
+}
+
 }
