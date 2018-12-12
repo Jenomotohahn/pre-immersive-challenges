1fbd56580393f757ae6efe74f254211657073052
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2018-12-11 19:29:43 -1000

    Changed the length -1 to just length

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,9 +1,9 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
-var newMultipleArr = [x];
-var newNum = x;
+var newMultipleArr = [];
+var newNum = 0;
 
-for( var i = 0; i < (length - 1); i++){
+for( var i = 0; i < length; i++){
   newNum += x
   newMultipleArr.push(newNum);
 }
